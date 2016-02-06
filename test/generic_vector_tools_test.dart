8b752c8058809ba891ac434719857350926c181d
BenLaptop <revolverator@hotmail.com>
2016-02-06 18:05:44 +0000

    cmd arg to disable print tests

--- test/generic_vector_tools_test.dart
+++ test/generic_vector_tools_test.dart
@@ -3,21 +3,33 @@
 
 library generic_vector_tools.test;
 
+import 'package:args/args.dart';
+
 import 'v2_tests.dart' as v2_tests;
 import 'v_tests.dart' as v_tests;
 import 'v_generic_tests.dart' as vg_tests;
 import 'm_tests.dart' as m_tests;
 import 'print_tests.dart' as print_tests;
 
-void main() {
+void main(List<String> args) {
   // To run tests:
   //   >dart "C:\DartDev\Vector\test\Vector_test.dart"
+
+  final ArgParser p = new ArgParser();
+  p.addFlag("disablePrintTests", abbr:"p");
+
+  var argResults = p.parse(args);
 
   runTests("V2", v2_tests.run);
   runTests("V", v_tests.run);
   runTests("V - Generics", vg_tests.run);
   runTests("M", m_tests.run);
-  runTests("Printing", print_tests.run);
+  if(argResults["disablePrintTests"]){
+    print('Skipping Print tests');
+  }
+  else{
+    runTests("Printing", print_tests.run);
+  }
 
   // Template
   // group('GroupName', () {
--- test/print_tests.dart
+++ test/print_tests.dart
@@ -12,28 +12,38 @@ void run(){
   group('Test printing', () {
     test('Test V.printVector()', () {
       V d = new V([0.0, 1.0]);
+      print("");
       expectTrue(d.printVector() == "(0.0, 1.0)");
+      print("");
       expectTrue(d.printVector(round: true) == "(0, 1)");
       V D = new V([d, d + 1.0, d + 2.0]);
+      print("");
       expectTrue(D.printVector() == "((0.0, 1.0), (1.0, 2.0), (2.0, 3.0))");
+      print("");
       expectTrue(D.printVector(round: true) == "((0, 1), (1, 2), (2, 3))");
       V DD = new V([D, D + 1.0, D + 2.0]);
+      print("");
       expectTrue(DD.printVector() ==
-          '(((0.0, 1.0), (1.0, 2.0), (2.0, 3.0)),' +
-              ' ((1.0, 2.0), (2.0, 3.0), (3.0, 4.0)),' +
+          '(((0.0, 1.0), (1.0, 2.0), (2.0, 3.0)),'
+              ' ((1.0, 2.0), (2.0, 3.0), (3.0, 4.0)),'
               ' ((2.0, 3.0), (3.0, 4.0), (4.0, 5.0)))');
+      print("");
     });
 
     test('Test M.printVector()', () {
       M O = new M.fromArray(2, 3, [1.5, 2.0, 3.0, 4.0, 5.0, 6.0]);
+      print("");
       expectStringsEqual(
           O.printMatrix(), '(1.5, 2.0, 3.0)\r\n(4.0, 5.0, 6.0)');
+      print("");
       expectStringsEqual(O.printMatrix(round: true), '(2, 2, 3)\r\n(4, 5, 6)');
+      print("");
       V v = new V([0.0, 1.0]);
       M A = new M.fromArray(
           2, 3, [v, v + 1.0, v + 2.0, v + 3.0, v + 4.0, v + 5.0]);
       expectStringsEqual(A.printMatrix(),
           '((0.0, 1.0), (1.0, 2.0), (2.0, 3.0))\r\n((3.0, 4.0), (4.0, 5.0), (5.0, 6.0))');
+      print("");
     });
   });
 }
