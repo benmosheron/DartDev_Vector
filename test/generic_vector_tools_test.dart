42c3531aebdf35185b489507b59f694c96efde4c
BenLaptop <revolverator@hotmail.com>
2016-04-03 13:36:20 +0100

    removed cmd flags so tests run with pub run test

--- test/generic_vector_tools_test.dart
+++ test/generic_vector_tools_test.dart
@@ -11,24 +11,14 @@ import 'v_generic_tests.dart' as vg_tests;
 import 'm_tests.dart' as m_tests;
 import 'print_tests.dart' as print_tests;
 
-void main(List<String> args) {
+void main() {
   // To run tests:
   //   >dart "C:\DartDev\Vector\test\Vector_test.dart"
-
-  final ArgParser p = new ArgParser();
-  p.addFlag("disablePrintTests", abbr: "p");
-
-  var argResults = p.parse(args);
-
   runTests("V2", v2_tests.run);
   runTests("V", v_tests.run);
   runTests("V - Generics", vg_tests.run);
   runTests("M", m_tests.run);
-  if (argResults["disablePrintTests"]) {
-    print('Skipping Print tests');
-  } else {
-    runTests("Printing", print_tests.run);
-  }
+  runTests("Printing", print_tests.run);
 
   // Template
   // group('GroupName', () {
