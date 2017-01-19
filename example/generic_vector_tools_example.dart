f5ce25ad4987ca4cc67ae007219e3de844387c7f
BenLaptop <revolverator@hotmail.com>
2017-01-19 17:39:20 +0000

    Fixed up example syntax.

--- example/generic_vector_tools_example.dart
+++ example/generic_vector_tools_example.dart
@@ -9,43 +9,43 @@ main() {
   // Create a vector of two doubles - a position
   V<double> position = new V<double>([0.0, 10.0]);
   print("position:");
-  position.Print();
+  position.printVector();
   print("");
 
   // Create a vector of positions
   V<V<double>> positions =
       new V<V<double>>([position, position + 10.0, position + 25.0]);
   print("positions:");
-  positions.Print();
+  positions.printVector();
   print("");
 
   // Calculate a matrix of distances between all positions
-  V distances = positions.Resolve(positions, (V p1, V p2) => p2 - p1);
+  V distances = positions.resolve(positions, (V p1, V p2) => p2 - p1);
   print("distances:");
-  distances.Print();
+  distances.printVector();
   print("");
 
   // M class is a wrapper around vector with some methods suited to 2D, like printing:
-  M distanceMatrix = new M.FromV(distances);
+  M distanceMatrix = new M.fromV(distances);
   print("distanceMatrix:");
-  distanceMatrix.Print();
+  distanceMatrix.printMatrix();
   print("");
 
   // We can also easily compute the magnitude of the distances (or any other function over elements)
-  M magnitudes = new M.FromV(
-      positions.Resolve(positions, (V p1, V p2) => (p2 - p1).Magnitude));
+  M magnitudes = new M.fromV(
+      positions.resolve(positions, (V p1, V p2) => (p2 - p1).Magnitude));
   print("magnitudes:");
-  magnitudes.Print();
+  magnitudes.printMatrix();
   print("");
 
   // For easy viewing:
   print("magnitudes (rounded for printing):");
-  magnitudes.Print(round: true);
+  magnitudes.printMatrix(round: true);
   print("");
 
   // And easily apply functions over the matrix
-  M magnitudesCubedThenRounded = magnitudes.MapF((e) => (e * e * e).round());
+  M magnitudesCubedThenRounded = magnitudes.mapF((e) => (e * e * e).round());
   print("magnitudesCubedThenRounded:");
-  magnitudesCubedThenRounded.Print();
+  magnitudesCubedThenRounded.printMatrix();
   print("");
 }
